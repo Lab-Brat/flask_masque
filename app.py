b136cc3e64ef60dfbbd9ba2e2645eb55f3ede086
Lab-Brat <xoovoox@protonmail.com>
2022-05-29 14:38:15 +0300

    small stylistic changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template
 from flask import request, redirect
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
