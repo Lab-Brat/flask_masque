32bf605d34567e0be12e091046456949459064eb
Lab-Brat <xoovoox@protonmail.com>
2022-06-04 23:41:08 +0300

    save extra ips to a separate table with foreign key

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, render_template
 from flask import request, redirect
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import INET
 from datetime import datetime
 
@@ -12,44 +13,58 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class CreateForm(db.Model):
-    __tablename__ = 'forms_table'
+    __tablename__ = 'forms'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     hostname = db.Column(db.String(30), nullable=False)
     ip = db.Column(INET)
-    extra_ips = db.Column(db.String(30), nullable=False)
+    extra_ips = db.relationship('CreateExIP', cascade="all,delete", backref='forms')
     functions = db.Column(db.String(30), nullable=False)
     subsystems = db.Column(db.String(30), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now)
 
-    def __init__(self, name, hostname, ip, extra_ips,
-                       functions, subsystems):
+    def __init__(self, name, hostname, ip, functions, subsystems):
         self.name = name
         self.hostname = hostname
         self.ip = ip
-        self.extra_ips = extra_ips
         self.functions = functions
         self.subsystems = subsystems
 
     def __repr__(self) -> str:
         return '<Form %r>' % self.id
 
+class CreateExIP(db.Model):
+    __tablename__ = 'extra_ips'
+
+    id = db.Column(db.Integer, primary_key=True)
+    forms_id = db.Column(db.Integer, ForeignKey("forms.id"))
+    extra_ip = db.Column(INET)
+
+    def __init__(self, forms_id, extra_ip) :
+        self.forms_id = forms_id
+        self.extra_ip = extra_ip
+    
+    def __repr__(self) -> str:
+        return '<ExIP %r>' % self.id
+
 @app.route('/form', methods=['POST', 'GET'])
 def form():
     if request.method == 'POST':
-        exips = request.form.getlist('field[]')
-        ips_s = ' '
-
         new_form = CreateForm(
                 name=request.form['name'],
                 hostname=request.form['hostname'],
                 ip=request.form['ip'],
-                extra_ips=ips_s.join(exips),
                 functions=request.form['functions'],
                 subsystems=request.form['subsystems'])
 
         db.session.add(new_form)
+        db.session.flush()
+
+        extra_ip = request.form.getlist('field[]')
+        new_extra_ips = [CreateExIP(forms_id=new_form.id, extra_ip=ip) for ip in extra_ip]
+        db.session.add_all(new_extra_ips)
+
         db.session.commit()
         return redirect('/')
 
