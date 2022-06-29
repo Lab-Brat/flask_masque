19c9b1798c940db405b7c1c6faad2ee1df2dd1fe
Lab-Brat <xoovoox@protonmail.com>
2022-06-29 10:20:33 +0300

    expanded bash script that populates db

--- samples/add_entries.sh
+++ samples/add_entries.sh
@@ -4,12 +4,20 @@ db_user=$(grep 'db_user' ../config.ini | cut -d" " -f3 | tr -d $'\r')
 db_name=$(grep 'db_name' ../config.ini | cut -d" " -f3 | tr -d $'\r')
 db_address=$(grep 'db_address' ../config.ini | cut -d" " -f3 | tr -d $'\r')
 db_port=$(grep 'db_port' ../config.ini | cut -d" " -f3 | tr -d $'\r')
-db_entries=$(grep 'sample_entries' ../config.ini | awk '{print $3}' | tr -d $'\r')
-db_columns="(name, hostname, ip, distro, functions, subsystems, date_created)"
 
+form_columns="(name, hostname, cluster_belong, ip, distro, functions, subsystems, date_created)"
+exip_columns="(forms_id, extra_ip)"
+cluster_columns="(cluster, description, cluster_functions,cluster_subsystems, date_created)"
+form_entries=$(grep 'form_entries' ../config.ini | awk '{print $3}' | tr -d $'\r')
+exip_entries=$(grep 'exip_entries' ../config.ini | awk '{print $3}' | tr -d $'\r')
+cluster_entries=$(grep 'cluster_entries' ../config.ini | awk '{print $3}' | tr -d $'\r')
 
 psql -U $db_user \
      -d $db_name \
      -h $db_address \
      -p $db_port \
-     -c "\copy forms $db_columns from $db_entries with DELIMITER ','"
+     -c "\copy forms $form_columns from $form_entries with DELIMITER ','" \
+     -c "\copy extra_ips $exip_columns from $exip_entries with DELIMITER ','" \
+     -c "\copy cluster_forms $cluster_columns from $cluster_entries with DELIMITER ','"
+
+echo "============= Database Populated ============="
