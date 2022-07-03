00aacd3026dcddda7d8e95ffc5e742ea5741987c
Lab-Brat <xoovoox@protonmail.com>
2022-07-03 16:50:21 +0300

    fix styling according to PEP8, renamed some variables

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import csv
 import json
 
 
+# ------------------------------ Load Configs ------------------------------- #
 config = configparser.ConfigParser()
 config.read_file(open('config.ini'))
 dump_path = config.get("User", "dump_path")
@@ -19,7 +20,8 @@ db_name = config.get("Database", "db_name")
 
 app = Flask(__name__)
 # define database engine, format: engine://user:password@host:port/database
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_address}:{db_port}/{db_name}'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f'postgresql://{db_user}:{db_password}@{db_address}:{db_port}/{db_name}')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'topsecretkey'
 dirlist = ['RedHat', 'Debian', 'Arch', 'SUSE', 'Gentoo', 'BSD']
@@ -27,20 +29,23 @@ dirlist = ['RedHat', 'Debian', 'Arch', 'SUSE', 'Gentoo', 'BSD']
 db.init_app(app)
 migrate = Migrate(app, db)
 
-#--------------------- main routes ---------------------#
+# ------------------------------- Main Routes ------------------------------- #
 # open new form input page after pressing "Add Form"
 @app.route('/form', methods=['POST', 'GET'])
 def form():
-    hosts = [instance[0] for instance in db.session.query(CreateForm.hostname)]
+    hosts_query = db.session.query(CreateForm.hostname)
 
+    unit_query = (db.session.query(CreateClusters.unit_name)
+                            .order_by(CreateClusters.unit_name))
+    
+    level_query = (db.session.query(CreateClusters.cluster_functions, 
+                                    CreateClusters.cluster_subsystems)
+                             .order_by(CreateClusters.unit_name))
 
-    unit_query = db.session.query(CreateClusters.unit_name).order_by(CreateClusters.unit_name)
+    hosts = [instance[0] for instance in hosts_query]
     orgunits = [unit[0] for unit in unit_query]
-
-    cluster_data = [list(instance) for instance in db.session.query(
-                        CreateClusters.cluster_functions, 
-                        CreateClusters.cluster_subsystems).order_by(CreateClusters.unit_name)]
-    cluster_dict = [[c, [cd[0], cd[1]]] for c, cd in zip(orgunits, cluster_data)]
+    level_data = [list(instance) for instance in level_query]
+    level_dict = [[c, [cd[0], cd[1]]] for c, cd in zip(orgunits, level_data)]
 
     if request.method == 'POST':
         new_form = CreateForm(
@@ -53,22 +58,27 @@ def form():
                 subsystems=request.form['subsystems'])
 
         if new_form.hostname in hosts:
-            return 'HOSTNAME EXISTS!!!\
-                    Next time please click on "Check Hostname" button before filling out the whole form!'
+            return ('HOSTNAME EXISTS!!!\n'
+                    'Next time please click on "Check Hostname"'
+                    'button before filling out the whole form!')
 
         db.session.add(new_form)
         db.session.flush()
 
         extra_ip = request.form.getlist('field[]')
-        new_extra_ips = [CreateExIP(forms_id=new_form.id, extra_ip=ip) for ip in extra_ip]
+        new_extra_ips = [CreateExIP(forms_id=new_form.id, 
+                                    extra_ip=ip) for ip in extra_ip]
         db.session.add_all(new_extra_ips)
 
         db.session.commit()
         return redirect('/')
 
     else:
-        return render_template('form.html', hosts=json.dumps(hosts), dirlist=dirlist,
-                                    clusters=orgunits, cluster_data=json.dumps(cluster_dict))
+        return render_template('form.html',
+                               dirlist=dirlist, 
+                               clusters=orgunits,
+                               hosts=json.dumps(hosts),
+                               cluster_data=json.dumps(level_dict))
 
 # delete form after pressing "Delete" link
 @app.route('/delete/<int:id>')
@@ -86,12 +96,20 @@ def delete(id):
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     form = CreateForm.query.get_or_404(id)
-    hosts = [instance[0] for instance in db.session.query(CreateForm.hostname)]
-    clusters = [instance[0] for instance in db.session.query(CreateClusters.cluster)]
-    cluster_data = [list(instance) for instance in db.session.query(
-                        CreateClusters.cluster_functions, 
-                        CreateClusters.cluster_subsystems)]
-    cluster_dict = [[c, [cd[0], cd[1]]] for c, cd in zip(clusters, cluster_data)]
+
+    hosts_query = db.session.query(CreateForm.hostname)
+
+    unit_query = (db.session.query(CreateClusters.unit_name)
+                            .order_by(CreateClusters.unit_name))
+    
+    level_query = (db.session.query(CreateClusters.cluster_functions, 
+                                    CreateClusters.cluster_subsystems)
+                             .order_by(CreateClusters.unit_name))
+
+    hosts = [instance[0] for instance in hosts_query]
+    orgunits = [unit[0] for unit in unit_query]
+    level_data = [list(instance) for instance in level_query]
+    level_dict = [[c, [cd[0], cd[1]]] for c, cd in zip(orgunits, level_data)]
 
     if request.method == 'POST':
         eips = request.form.getlist('extra_ips[]')
@@ -110,7 +128,8 @@ def update(id):
         form.subsystems=request.form['subsystems']
 
         extra_ip = request.form.getlist('field[]')
-        new_extra_ips = [CreateExIP(forms_id=form.id, extra_ip=ip) for ip in extra_ip]
+        new_extra_ips = [CreateExIP(forms_id=form.id, 
+                                    extra_ip=ip) for ip in extra_ip]
         db.session.add_all(new_extra_ips)
 
         try:
@@ -119,36 +138,45 @@ def update(id):
         except:
             return "Failed to Update Form"
     else:
-        return render_template('update.html', form=form, hosts=json.dumps(hosts), dirlist=dirlist, 
-                                clusters=clusters, cluster_data=json.dumps(cluster_dict))
+        return render_template('update.html', 
+                               dirlist=dirlist, 
+                               clusters=orgunits,
+                               hosts=json.dumps(hosts),
+                               cluster_data=json.dumps(level_dict))
 
 # Save all database data into csv file
 @app.route('/dump', methods=['GET'])
 def dump():
-    exip_dict = {}
-    for instance in db.session.query(CreateExIP).order_by(CreateExIP.id):
+    exip_dict = dict
+    extra_ip_query = db.session.query(CreateExIP).order_by(CreateExIP.id)
+
+    for instance in extra_ip_query:
         if str(instance.forms_id) in exip_dict:
             exip_dict[str(instance.forms_id)] += '\r\n' + instance.extra_ip
         else:
             exip_dict[str(instance.forms_id)] = instance.extra_ip
 
-    timestamp = f"{str(datetime.now())[0:10]}_{str(datetime.now())[12:19]}"
+    now = datetime.now().replace(microsecond=0)
+    timestamp = f"{now.date()}_{now.time()}".replace(':','-')
     dump_file = f'{dump_path}/dump_{timestamp}.csv'
-    header = ['Name', 'Hostname', 'IP', 'Extra IPs', 'Functions', 'Subsystems']
+    header = ['Name', 'Hostname', 'IP', 
+              'Extra IPs', 'Functions', 'Subsystems']
 
     with open(dump_file, 'w', encoding='UTF8') as dump:
         writer = csv.writer(dump)
         writer.writerow(header)
-        for instance in db.session.query(CreateForm).order_by(CreateForm.id):
+        form_query = db.session.query(CreateForm).order_by(CreateForm.id)
+        for instance in form_query:
             try:
                 exip_csv = exip_dict[str(instance.id)]
             except:
                 exip_csv = ''
-            row_data = [ instance.name, instance.hostname, instance.ip, exip_csv, 
-                         instance.functions, instance.subsystems]
+            row_data = [instance.name, instance.hostname, instance.ip, 
+                        exip_csv, instance.functions, instance.subsystems]
             writer.writerow(row_data)
 
-    return send_file(dump_file, mimetype='text/csv', download_name='db_dump.csv')
+    return send_file(dump_file, mimetype='text/csv', 
+                     download_name='db_dump.csv')
 
 # main page
 @app.route('/', methods=['GET', 'POST'])
@@ -157,18 +185,19 @@ def index():
     return render_template('index.html', forms=forms)
 
 
-#--------------------- cluster routes ---------------------#
+# ---------------------------- Org. Unit Routes ----------------------------- #
 @app.route('/cluster_new', methods=['POST', 'GET'])
 def cluster_new():
     if request.method == 'POST':
-        new_cluster = CreateClusters(unit_name=request.form['unit_name'],
-                            unit_level=request.form['unit_level'],
-                            description=request.form['description'],
-                            cluster=request.form['cluster'],
-                            containerization=request.form['containerization'],
-                            pods=request.form['pods'],
-                            cluster_functions=request.form['cluster_functions'],
-                            cluster_subsystems=request.form['cluster_subsystems'])
+        new_cluster = CreateClusters(
+                        unit_name=request.form['unit_name'],
+                        unit_level=request.form['unit_level'],
+                        description=request.form['description'],
+                        cluster=request.form['cluster'],
+                        containerization=request.form['containerization'],
+                        pods=request.form['pods'],
+                        cluster_functions=request.form['cluster_functions'],
+                        cluster_subsystems=request.form['cluster_subsystems'])
 
         db.session.add(new_cluster)
         db.session.commit()
@@ -215,9 +244,13 @@ def cluster_delete(id):
 
 @app.route('/cluster', methods=['POST', 'GET'])
 def cluster():
-    clusters = CreateClusters.query.order_by(CreateClusters.date_created).all()
-    hosts = [instance for instance in db.session.query(CreateForm.hostname, CreateForm.cluster_belong)]
-    
+    hosts_query = db.session.query(CreateForm.hostname, 
+                                   CreateForm.cluster_belong)
+
+    clusters = (CreateClusters.query
+                              .order_by(CreateClusters.date_created).all())
+
+    hosts = [instance for instance in hosts_query]
     hc = {cl.unit_name: list() for cl in clusters}
     [hc[h[1]].append(h[0]) for h in hosts if h[1] in hc.keys()]
 
