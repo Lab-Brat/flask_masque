16fd5c3ade1118427c7447d1262e57ffabd743e3
LabBrat <xoovoox@protonmail.com>
2022-07-27 22:56:10 +0300

    turn env.sh into .env for docker compose

--- env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export DB_USER=boink
-export DB_PASS=password
-export DB_ADDRESS=flask_masque_masq_db_1
-export DB_PORT=5432
-export DB_NAME=masq_forms
-
-export DB_URI="postgresql://$DB_USER:$DB_PASS@$DB_ADDRESS:$DB_PORT/$DB_NAME"
