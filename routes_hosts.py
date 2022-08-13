f0331b8aec37865fa5e4ced94a1a578603abbcc8
LabBrat <xoovoox@protonmail.com>
2022-08-13 22:18:16 +0300

    stylistic changes

--- routes_hosts.py
+++ routes_hosts.py
@@ -11,17 +11,17 @@ dirlist = Tools().dirlist
 routes_hosts = Blueprint("routes_hosts", __name__)
 
 # open form to register host information
-@routes_hosts.route('/form', methods=['POST', 'GET'])
+@routes_hosts.route('/form', methods = ['POST', 'GET'])
 def form():
     if request.method == 'POST':
         new_form = CreateForm(
-                name=request.form['name'],
-                hostname=request.form['hostname'],
-                unit_belong=request.form['unit_belong'],
-                ip=request.form['ip'],
-                distro=request.form['distro'],
-                functions=request.form['functions'],
-                subsystems=request.form['subsystems'])
+                name = request.form['name'],
+                hostname = request.form['hostname'],
+                unit_belong = request.form['unit_belong'],
+                ip = request.form['ip'],
+                distro = request.form['distro'],
+                functions = request.form['functions'],
+                subsystems = request.form['subsystems'])
 
         if Tools().check_host_existence(new_form) is False:
             return ('HOSTNAME EXISTS!!!'
@@ -32,18 +32,18 @@ def form():
         db.session.flush()
 
         extra_ip = request.form.getlist('field[]')
-        new_extra_ips = [CreateExIP(forms_id=new_form.id, 
-                                    extra_ip=ip) for ip in extra_ip]
+        new_extra_ips = [CreateExIP(forms_id = new_form.id, 
+                                    extra_ip = ip) for ip in extra_ip]
         db.session.add_all(new_extra_ips)
         db.session.commit()
         return redirect('/')
 
     else:
         return render_template('form.html',
-                        dirlist=dirlist, 
-                        units=DB_Tools(db).unit_query(),
-                        hosts=json.dumps(DB_Tools(db).host_query()),
-                        unit_data=json.dumps(DB_Tools(db).data_query()))
+                        dirlist = dirlist, 
+                        units = DB_Tools(db).unit_query(),
+                        hosts = json.dumps(DB_Tools(db).host_query()),
+                        unit_data = json.dumps(DB_Tools(db).data_query()))
 
 #delete form after pressing "Delete" link
 @routes_hosts.route('/delete/<int:id>')
@@ -54,18 +54,18 @@ def delete(id):
     return redirect('/')
 
 # open update page after pressing "Update" link
-@routes_hosts.route('/update/<int:id>', methods=['GET', 'POST'])
+@routes_hosts.route('/update/<int:id>', methods = ['GET', 'POST'])
 def update(id):
     form = CreateForm.query.get_or_404(id)
 
     if request.method == 'POST':
-        form.name=request.form['name']
-        form.hostname=request.form['hostname']
-        form.unit_belong=request.form['unit_belong'],
-        form.ip=request.form['ip']
-        form.distro=request.form['distro']
-        form.functions=request.form['functions']
-        form.subsystems=request.form['subsystems']
+        form.name = request.form['name']
+        form.hostname = request.form['hostname']
+        form.unit_belong = request.form['unit_belong'],
+        form.ip = request.form['ip']
+        form.distro = request.form['distro']
+        form.functions = request.form['functions']
+        form.subsystems = request.form['subsystems']
 
         # change existring extra IPs
         exip_form = request.form.getlist('extra_ips[]')
@@ -76,46 +76,47 @@ def update(id):
 
         # process newly created extra IPs
         extra_ip = request.form.getlist('field[]')
-        new_extra_ips = [CreateExIP(forms_id=form.id, 
-                                    extra_ip=ip) for ip in extra_ip]
+        new_extra_ips = [CreateExIP(forms_id = form.id, 
+                                    extra_ip = ip) for ip in extra_ip]
 
         db.session.add_all(new_extra_ips)
         db.session.commit()
         return redirect('/')
 
     else:
-        return render_template('update.html', form=form, dirlist=dirlist, 
-                            units=DB_Tools(db).unit_query(),
-                            hosts=json.dumps(DB_Tools(db).host_query()),
-                            unit_data=json.dumps(DB_Tools(db).data_query()))
+        return render_template('update.html', form = form, dirlist = dirlist,
+                        units = DB_Tools(db).unit_query(),
+                        hosts = json.dumps(DB_Tools(db).host_query()),
+                        unit_data = json.dumps(DB_Tools(db).data_query()))
 
 # Save all database data into csv file
-@routes_hosts.route('/dump', methods=['GET'])
+@routes_hosts.route('/dump', methods = ['GET'])
 def dump():
     dump_file = f'./dumps/dump_{Tools().timestamp()}.csv'
-    os.makedirs(os.path.dirname(dump_file), exist_ok=True)
+    os.makedirs(os.path.dirname(dump_file), exist_ok = True)
 
     Tools().write_csv(dump_file)
 
-    return send_file(dump_file, mimetype='text/csv', 
-                     download_name='db_dump.csv')
+    return send_file(dump_file, mimetype = 'text/csv', 
+                     download_name = 'db_dump.csv')
 
 # Upload hosts infromation to the app
-@routes_hosts.route('/upload_csv', methods=['GET', 'POST'])
+@routes_hosts.route('/upload_csv', methods = ['GET', 'POST'])
 def upload_csv():
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename).split('.')
-        if Tools()._extension_check(filename) is True:
+
+        if Tools()._extension_check(filename):
             filename = filename[0] + f"_{Tools().timestamp()}" + ".csv"
-            os.makedirs(os.path.dirname('uploads/'), exist_ok=True)
+            os.makedirs(os.path.dirname('uploads/'), exist_ok = True)
             file.save(os.path.join('uploads/', filename))
 
             header, content = Tools()._read_csv(filename)
             new_forms = Tools().extract_csv_form(header, content)
 
             for form in new_forms:
-                if Tools().check_host_existence(form) is True:
+                if Tools().check_host_existence(form):
                     try:
                         db.session.add(form)
                     except:
@@ -133,7 +134,7 @@ def upload_csv():
     return render_template('upload_csv.html')
 
 # main page
-@routes_hosts.route('/', methods=['GET', 'POST'])
+@routes_hosts.route('/', methods = ['GET', 'POST'])
 def index():
     forms = DB_Tools(db).get_model('form')
-    return render_template('index.html', forms=forms)
+    return render_template('index.html', forms = forms)
