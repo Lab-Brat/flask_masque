b4a8aed2b30a4088b25c5486183cb1b4ccdc19f2
LabBrat <xoovoox@protonmail.com>
2022-08-17 23:43:20 +0300

    change delete extra email to button

--- static/js/add_ip.js
+++ static/js/add_ip.js
@@ -2,7 +2,14 @@ $(document).ready(function(){
   var max_input_fields = 10;
   var add_input = $('.add-input');
   var input_wrapper = $('.input-wrapper');
-  var new_input = '<div><input type="text" name="field[]" value=""/><a href="javascript:void(0);" class="remove-input" title="Remove input">Delete</a></div>';
+  var new_input = '\
+  <div>\
+  <input type="text" name="field[]" value=""/>\
+  <a href="javascript:void(0);" class="remove-input" title="Remove input">\
+  <button type="button">Delete</button>\
+  </a>\
+  </div>';
+
   var add_input_count = 1; 
   
   $(add_input).click(function(){
