1546eea9a35899427755c850a49aa1999c075108
LabBrat <xoovoox@protonmail.com>
2022-08-21 21:49:06 +0300

    remove manually implementedsearch

--- routes_service.py
+++ routes_service.py
@@ -7,13 +7,3 @@ from tools import Tools, DB_Tools
 T = Tools()
 DBT = DB_Tools(db)
 routes_service = Blueprint("routes_service", __name__)
-
-@routes_service.route('/search', methods = ['GET', 'POST'])
-def search():
-    if request.method == "POST":
-        query = request.form['query']
-        result = [h for h in DBT.host_query() if query in h]
-        host_dict = DBT.get_form_dict()
-        forms = [host_dict[r] for r in result]
-        return render_template('search.html', forms = forms)
-    return render_template('search.html')
